c266751b2677689f7ad6ea107cf7ba465456911c
ylafaro0310 <y16mukurou@gmail.com>
2020-04-28 12:53:04 +0900

    First commit

---
+++ stories/index.stories.js
@@ -0,0 +1,10 @@
+import './_index.scss';
+import button from './button.html';
+
+export default {
+  title: 'Demo',
+};
+
+export const Heading = () => '<h1>Hello World</h1>';
+
+export const Button = () => button;
