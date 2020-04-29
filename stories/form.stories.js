0ea1d08515fc7c19ac45b47a8f773b1f846e4cd3
ylafaro0310 <y16mukurou@gmail.com>
2020-04-29 15:45:33 +0900

    sb: Add my-habit component

---
+++ stories/form.stories.js
@@ -0,0 +1,11 @@
+import './form.scss';
+import './card.scss';
+import form from './form.html';
+import card from './card.html';
+
+export default {
+  title: 'Form',
+};
+
+export const Form = () => form;
+export const Card = () => card;
